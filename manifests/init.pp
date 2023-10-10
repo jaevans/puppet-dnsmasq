08b8c39c58f7477a6292a3d9341f252147fb5ba0
James Evans <54334+jaevans@users.noreply.github.com>
2023-10-09 22:33:28 -0500

    Documentation for the main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,128 @@
-# Primary class with options.  See documentation at
-# http://www.thekelleys.org.uk/dnsmasq/docs/dnsmasq-man.html
+# @summary Manage dnsmasq
+#
+# @example
+#   class { 'dnsmasq':
+#     interface         => 'lo',
+#     listen_address    => '192.168.39.1',
+#     no_dhcp_interface => '192.168.49.1',
+#     domain            => 'int.lan',
+#     port              => '53',
+#     expand_hosts      => true,
+#     enable_tftp       => true,
+#     tftp_root         => '/var/lib/tftpboot',
+#     dhcp_boot         => 'pxelinux.0',
+#     dns_forward_max   => 1500,
+#     domain_needed     => true,
+#     bogus_priv        => true,
+#     no_negcache       => true,
+#     no_hosts          => true,
+#     resolv_file       => '/etc/resolv.conf',
+#     cache_size        => 1000,
+#     restart           => true,
+#   }
+#
+# @see http://www.thekelleys.org.uk/dnsmasq/docs/dnsmasq-man.html
+# 
+# @param dnsmasq_confdir
+#   Read all the files in the given directory as configuration files.
+# @param dnsmasq_conffile
+#   The location of the dnsmasq configuration file.
+# @param dnsmasq_hasstatus
+#   Whether the init script has status support.
+# @param dnsmasq_logdir
+#   The location of the dnsmasq log directory.
+# @param dnsmasq_package
+#   The name of the dnsmasq package.
+# @param dnsmasq_service
+#   The name of the dnsmasq service.
+# @param dnsmasq_package_provider
+#   Override the package provider for dnsmasq.
+# @param auth_sec_servers
+#  Specify any secondary servers for a zone for which dnsmasq is authoritative.
+# @param auth_server
+#   Enable authoritative DNS for queries on the specified interface or address.
+# @param auth_ttl
+#   Specify the TTL for DNS records served from the authoritative DNS server.
+# @param auth_zone
+#   Define a DNS zone for which dnsmasq acts as authoritative server.
+# @param bogus_priv
+#   Reject DNS responses which contain private IP addresses.
+# @param cache_size
+#   Set the size of dnsmasq's cache.
+# @param config_hash
+#   A hash of additional configuration options. This can be used to specify any additional
+#   dnsmasq configuration that is not provided by the class's parameters. 
+#   The hash keys are the dnsmasq configuration option names, and the hash values are the configuration to use
+#   for that option. For example:
+#   ```
+#   class { 'dnsmasq':
+#     config_hash => {
+#       'dhcp-fqdn'          => true,
+#        'dhcp-authoritative' => true,
+#     },
+#   }
+#   ```
+# @param dhcp_boot
+#   Set the boot filename for netboot clients.
+# @param dhcp_leasefile
+#   Specify the location of the DHCP lease file.
+# @param dhcp_no_override
+#   Disable re-use of the DHCP servername and filename fields as extra option space.
+# @param domain
+#   Specify the domain name to be given to DHCP clients.
+# @param domain_needed
+#   Tell dnsmasq to never forward plain names (without a domain) to upstream nameservers.
+# @param dns_forward_max
+#   Set the maximum number of concurrent DNS queries.
+# @param enable_tftp
+#   Enable the built-in TFTP server.
+# @param expand_hosts
+#   Add the domain to plain names.
+# @param interface
+#   Specify the interface dnsmasq should listen on.
+# @param listen_address
+#   Specify the address dnsmasq should listen on. Both `listen_address` and `interface` can be specified.
+# @param local_ttl
+#   Set the TTL for DNS records served from /etc/hosts or the DHCP lease file. This should normally be set to 0.
+# @param max_ttl
+#   Set the maximum TTL for DNS records served to clients. If the true TTL is greater than the `max_ttl`, then
+#   the TTL is reduced to the `max_ttl`.
+# @param max_cache_ttl
+#   Set the maximum TTL for records in the cache.
+# @param neg_ttl
+#   Set the TTL for negative responses from upstream servers that do not provide a TTL from the SOA.
+# @param no_dhcp_interface
+#   Specify an interface that should not be used for DHCP or TFTP, only DNS.
+# @param no_hosts
+#   Disable reading of /etc/hosts.
+# @param no_negcache
+#   Disable caching of negative responses.
+# @param no_resolv
+#   Disable reading of /etc/resolv.conf.
+# @param port
+#  Specify the port dnsmasq should listen on.
+# @param read_ethers
+#   Read the Ethernet address from /etc/ethers and use it to set the DHCP provided hostname or IP address.
+# @param reload_resolvconf
+#   Use resolvconf(8) to notify clients of changes to /etc/resolv.conf.
+# @param resolv_file
+#   Read the IP addresses of the upstream nameservers from this file, instead of /etc/resolv.conf.
+# @param restart
+#   Whether to restart the dnsmasq service when the configuration changes.
+# @param run_as_user
+#   Specify the user to run dnsmasq as.
+# @param save_config_file
+#   Whether to save the commented default config file after installation.
+# @param service_enable
+#   Whether the dnsmasq service should be enabled.
+# @param service_ensure
+#   Whether the dnsmasq service should be running or stopped.
+# @param strict_order
+#   Require DNS servers to be queried in the order specified by the `resolv_file`.
+# @param manage_tftp_root
+#   Whether to manage the TFTP root directory.
+# @param tftp_root
+#   Specify the root directory for files available via TFTP.
 class dnsmasq (
   Stdlib::Absolutepath $dnsmasq_confdir,
   Stdlib::Absolutepath $dnsmasq_conffile,
