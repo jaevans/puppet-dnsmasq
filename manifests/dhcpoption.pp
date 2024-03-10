56867c6cec179f8cd8ba1c81c29c7ce434a9b645
James Evans <54334+jaevans@users.noreply.github.com>
2024-03-10 12:12:36 -0500

    Convert dhcpoption template to epp and document class

--- manifests/dhcpoption.pp
+++ manifests/dhcpoption.pp
@@ -1,4 +1,25 @@
-# Create an dnsmasq dhcp option (--dhcp-option).
+# @summary Configure DHCP options for dnsmasq
+#
+# This definition allows for the creation and management of dnsmasq DHCP options,
+# providing a way to specify options with or without tags, and handling numeric
+# and named options appropriately.
+#
+# @param content The content of the DHCP option to be set.
+# @param option The name of the DHCP option.
+# If not explicitly provided, it defaults to the resource's title.
+# Numeric options and those starting with 'option:' are handled specifically.
+# @param tag An optional tag to associate with the DHCP option. If not provided, no tag is applied.
+#
+# @example Create a DHCP option with a tag
+#   dnsmasq::dhcpoption { 'example-option':
+#     content => 'example-content',
+#     option  => 'example-option-name',
+#     tag     => 'example-tag',
+#   }
+#
+# @see dnsmasq The main dnsmasq class that must be included before using this resource type.
+# @see concat::fragment
+# The concat::fragment resource type used for managing parts of the dnsmasq configuration file.
 define dnsmasq::dhcpoption (
   String[1] $content,
   String[1] $option = $title,
@@ -25,6 +46,10 @@ define dnsmasq::dhcpoption (
   concat::fragment { "dnsmasq-dhcpoption-${tag}-${option}":
     order   => '03',
     target  => 'dnsmasq.conf',
-    content => template('dnsmasq/dhcpoption.erb'),
+    content => epp('dnsmasq/dhcpoption.epp', {
+        tag_real    => $tag_real,
+        option_real => $option_real,
+        content     => $content,
+    }),
   }
 }
